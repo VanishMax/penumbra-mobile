58eaaf555c14ec680a03b0d4ff5c8290210db8fb
Max Korsunov <maxkors2014@gmail.com>
2024-12-04 13:39:06 +0400

    feat: implement getting block height

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,16 @@
+use std::sync::Arc;
 use penumbra_keys::FullViewingKey;
 use penumbra_keys::keys::SpendKey;
 use penumbra_view::ViewServer;
+use tauri::Manager;
+use tauri::async_runtime::Mutex;
 
 mod view_server;
 
 struct AppState {
-    view_server: ViewServer,
-    spend_key: SpendKey,
-    full_viewing_key: FullViewingKey,
+    view_server: Option<Arc<ViewServer>>,
+    spend_key: Option<Arc<SpendKey>>,
+    full_viewing_key: Option<Arc<FullViewingKey>>,
 }
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -19,6 +22,14 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            app.manage(Mutex::new(AppState {
+                view_server: None,
+                spend_key: None,
+                full_viewing_key: None,
+            }));
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet, view_server::get_block_height, view_server::generate_keys])
         .run(tauri::generate_context!())
--- src-tauri/src/view_server.rs
+++ src-tauri/src/view_server.rs
@@ -1,46 +1,75 @@
+use std::ops::Deref;
 use std::str::FromStr;
+use std::sync::Arc;
+use penumbra_keys::FullViewingKey;
+use warp::Filter;
 use penumbra_keys::keys::{Bip44Path, SeedPhrase, SpendKey};
 use penumbra_view::ViewServer;
 use penumbra_proto::DomainType;
+use penumbra_proto::view::v1::view_service_server::ViewServiceServer;
 use tauri::{AppHandle, Manager, State};
 use tauri::async_runtime::Mutex;
 
 const ENDPOINT: &str = "https://testnet.plinfra.net";
 
 #[tauri::command]
-pub async fn generate_keys(app_handle: AppHandle, seed_phrase: &str) -> Result<(Vec<u8>, Vec<u8>), String> {
+pub async fn generate_keys(app_handle: AppHandle, state: State<'_, Mutex<crate::AppState>>, seed_phrase: &str) -> Result<(Vec<u8>, Vec<u8>), String> {
     let seed = match SeedPhrase::from_str(seed_phrase) {
         Ok(seed) => seed,
         Err(e) => return Err(format!("Error parsing seed phrase: {}", e)),
     };
     let path = Bip44Path::new(0);
 
-    let spend_key = SpendKey::from_seed_phrase_bip44(seed, &path);
-    let fvk = spend_key.full_viewing_key();
+    let spend_key: Arc<SpendKey> = Arc::new(SpendKey::from_seed_phrase_bip44(seed, &path));
+    let full_viewing_key: Arc<FullViewingKey> = Arc::new(spend_key.full_viewing_key().clone());
+    let result = (spend_key.encode_to_vec(), full_viewing_key.encode_to_vec());
+
+    let view_server = match ViewServer::load_or_initialize(
+        None::<&str>,
+        None::<&str>,
+        &full_viewing_key,
+        ENDPOINT.parse().unwrap(),
+    ).await {
+        Ok(vs) => Arc::new(vs),
+        Err(e) => return Err(format!("Error initializing view server: {}", e)),
+    };
+
+    let mut state = state.lock().await;
+    state.spend_key = Some(spend_key.clone());
+    state.full_viewing_key = Some(full_viewing_key.clone());
+    state.view_server = Some(view_server.clone());
     
-    app_handle.manage(crate::AppState {
-        spend_key: spend_key.clone(),
-        full_viewing_key: fvk.clone(),
-        view_server: match ViewServer::load_or_initialize(
-            None::<&str>,
-            None::<&str>,
-            &fvk,
-            ENDPOINT.parse().unwrap(),
-        ).await {
-            Ok(vs) => vs,
-            Err(e) => return Err(format!("Error initializing view server: {}", e)),
-        },
+    tokio::spawn(async move {
+        let vs_tonic = ViewServiceServer::from_arc(view_server.clone());
+        let server = tonic::transport::Server::builder()
+            .accept_http1(true)
+            .add_service(tonic_web::enable(vs_tonic))
+            .serve("127.0.0.1:3333".parse().unwrap());
+    
+        if let Err(e) = server.await {
+            Err(format!("Error running tonic server: {}", e))
+        } else { 
+            Ok(())
+        }
     });
     
-    Ok((
-        spend_key.encode_to_vec(),
-        fvk.encode_to_vec(),
-    ))
+    // Spawn the Warp server in the background.
+    tokio::spawn(async {
+        let routes = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+        warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    });
+    
+    Ok(result)
 }
 
 #[tauri::command]
 pub async fn get_block_height(state: State<'_, Mutex<crate::AppState>>) -> Result<u64, String> {
-    let view_server = &state.lock().await.view_server;
+    let view_server = match &state.lock().await.view_server {
+        Some(vs) => vs.deref().clone(),
+        None => {
+            return Err("View server not initialized".to_string());
+        }
+    };
     
     let block_height = match view_server.latest_known_block_height().await {
         Ok(height) => {
